0f9b85fe4fd22e89c6070609a70df94cc0dc5887
Alex-Choi <choijeaho86@gmail.com>
2021-08-06 23:57:35 +0900

    TypeORM 셋팅 및 MySQL 연결

---
+++ src/users/users.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    // pk number 생성
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // 컬럼 생성 : email
+    @Column({nullable:false})
+    email: string;
+
+    // 컬럼 생성 : nicknam
+    @Column({nullable:false, length: 10})
+    nicknam: string;
+
+    // 컬럼 생성 : password
+    @Column()
+    password: string;
+
+}
