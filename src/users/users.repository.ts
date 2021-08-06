f677d61e6c991e49845ac23ec216e056f60e586c
Alex-Choi <choijeaho86@gmail.com>
2021-08-07 08:47:57 +0900

    login code

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { EntityRepository, Repository } from 'typeorm';
+import { createToken } from '../function/token/tokenFun';
 import { User } from './users.entity';
 require('dotenv').config()
 
@@ -25,6 +26,33 @@ export class UserRepository extends Repository<User> {
 
     return "OK";
 
+  }
+
+  async signinUser(email : string, password : string): Promise <object> {
+
+    const user = new User();
+    const found = await this.findOne({email});
+    if(found){
+
+      if(!bcrypt.compareSync(password, found.password)){
+        // 로그인 에러처리 2 : 비밀번호가 틀렸을 경우(403)
+        throw new HttpException('해당 비밀번호가 틀렸습니다.', HttpStatus.FORBIDDEN);
+      }
+    }
+
+    else{
+      // 로그인 에러처리 1 : 존재하지 않는 이메일일 경우(403)
+      throw new HttpException('해당 이메일은 존재하지 않습니다.', HttpStatus.FORBIDDEN);
+    }
+
+    const token = createToken({email, nickname: found.nickname});
+
+    delete found.password;
+
+    return {
+      token,
+      user: found
+    };
   }
 
 }
