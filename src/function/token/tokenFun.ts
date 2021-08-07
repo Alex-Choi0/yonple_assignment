fed6a252363709948aadaf77244923bd0b3d267f
Alex-Choi <choijeaho86@gmail.com>
2021-08-07 14:14:29 +0900

    change tokenFun

--- src/function/token/tokenFun.ts
+++ src/function/token/tokenFun.ts
@@ -2,7 +2,6 @@ import { HttpException, HttpStatus } from "@nestjs/common";
 
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
-// import { User } from "../../users/users.entity";
 
 export const createToken = (payload : object) : string => {
         const tokenValue = jwt.sign(payload, process.env.SALT, {expiresIn : process.env.EXPIRESIN});
