d63c0035b5396cfa1924e7f3615b20139987be2d
Alex-Choi <choijeaho86@gmail.com>
2021-08-07 20:38:11 +0900

    code change

--- src/function/removeKeys.ts
+++ src/function/removeKeys.ts
@@ -1,4 +1,4 @@
-export const removeKeys = (ele: any, arr: Array<string>) : any => {
+export const removeKeys = (ele: any, arr: Array<string>) : object => {
 
   for(let i = 0; i < arr.length; i++){
     delete ele.user[arr[i]]
